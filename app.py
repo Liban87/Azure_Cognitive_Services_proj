46342fed9e2db3961059277f96dcfdc941c35a70
Ivan Arias <lialbertoarias@gmail.com>
2023-05-31 12:16:26 -0500

    Homework base program app.py modified to use azure cognitive services with the detect_language and translate functions.

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, request, render_template
+import os
+import requests, json
+
+global translator_endpoint    
+global cog_key    
+global cog_region
+
+try:
+    cog_key = 'e2020d9ab7b047ca94f505e31791b6d1'#os.environ.get("COG_SERVICE_KEY")
+    cog_region = 'eastus' #os.environ.get("COG_SERVICE_REGION")      
+    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
+except Exception as ex:        
+    print(ex)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        text = request.form['text']
+        # Aquí es donde procesarías el texto.
+        # Detect the language
+        language = GetLanguage(text)
+        print('Language:',language)
+
+        # Translate if not already English
+        if language != 'en':
+            translation = Translate(text, language)
+            print("\nTranslation:\n{}".format(translation))
+
+        return render_template('home.html', translated_text=translation,lang_detected=language)
+    
+    return render_template('home.html')
+
+def GetLanguage(text):
+    # Default language is English
+    language = 'en'
+
+    # Use the Translator detect function
+    path = '/detect'
+    url = translator_endpoint + path
+
+    # Build the request
+    params = {
+        'api-version': '3.0'
+    }
+
+    headers = {
+    'Ocp-Apim-Subscription-Key': cog_key,
+    'Ocp-Apim-Subscription-Region': cog_region,
+    'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get language
+    language = response[0]["language"]
+
+    # Return the language
+    return language
+
+def Translate(text, source_language):
+    translation = ''
+
+    # Use the Translator translate function
+    path = '/translate'
+    url = translator_endpoint + path
+
+    # Build the request
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['en']
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get translation
+    translation = response[0]["translations"][0]["text"]
+
+    # Return the translation
+    return translation
+
+if __name__ == "__main__":
+    app.run(debug=True)
